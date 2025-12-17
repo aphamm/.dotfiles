8658d3a38ba37728e1e82da6e89836277fcad9f9
austin pham <austinpham77@gmail.com>
2025-12-17 10:30:32 -0800

    Remove Node.js and nvm installation from bootstrap.sh
    Removed Node.js and nvm installation steps from bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -60,18 +60,6 @@ fi
 # Install Ruff globally
 uv tool install ruff@latest
 
-# Download and install nvm:
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-
-# in lieu of restarting the shell
-\. "$HOME/.nvm/nvm.sh"
-
-# Download and install Node.js:
-nvm install 25
-
-# Verify the Node.js version:
-node -v # Should print "v25.2.1".
-
 # Install Corepack:
 npm install -g corepack
 
